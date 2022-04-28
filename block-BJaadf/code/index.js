2434ff862441697147778477139f6f5960d0d591
Rajnish <mr.rajnish2000@gmail.com>
2022-04-28 09:58:08 +0530

    block-BJaadf

--- block-BJaadf/code/index.js
+++ block-BJaadf/code/index.js
@@ -1,21 +1,21 @@
 let fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
 
 // Add a new element ('Papaya') into fruits array at end
-
+fruits.push("papaya");
 // Remove the last element from fruits array
-
+fruits.pop();
 // Log the current length of fruits array
-
+console.log(fruits.length);
 // Remove the first element from fruits and log the value of fruits (use delete arr[0])
-
+fruits.shift();
 // Log the element on index 0 and 1
-
+console.log(fruits.chart[0]);
 // If the element at 0 index is undefined and index 1 is 'Banana' shift all the values to one lower index (use shift method)
-
+fruits.shift();
 // Add a new element to the start ('Guava') and shift the index of all other to one higher value
-
+fruits.unshift("Guava");
 // Add a new element to the start ('Dragon Fruit') and shift the index of all other to one higher value
-
+fruits.unshift("Dragan fruits");
 // Log true or false based on the value at index 0 and 1 is 'Dragon Fruit' and 'Guava' or not
 
 // Update the value of index 1 to `Pears`
